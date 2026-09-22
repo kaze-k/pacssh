98bb89f3e8509bf7707f762e3ae4f5f215925226
kaze-k <kerkaze@outlook.com>
2026-09-22 22:57:25 +0800

    perf

--- src/main.rs
+++ src/main.rs
@@ -23,16 +23,20 @@ fn main() {
     let from = modules::From::new(&ssh.client_ip, &ssh.client_port);
     let to = modules::To::new(&ssh.server_ip, &ssh.server_port);
 
-    let lines: Vec<Box<dyn Module>> = vec![
+    let mut lines: Vec<Box<dyn Module>> = vec![
         Box::new(blank),
         Box::new(login),
         Box::new(session),
         Box::new(blank),
         Box::new(tty),
-        Box::new(from),
-        Box::new(to),
-        Box::new(blank),
     ];
+
+    if ssh.is_ssh {
+        lines.push(Box::new(from));
+        lines.push(Box::new(to));
+    }
+
+    lines.push(Box::new(blank));
 
     let renderer = render::Renderer::new();
 
